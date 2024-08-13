ac97dec7048133427b42d362bad0f67eb7674e82
Faishal Mukhammad Syarief <faishalmukhammadsyarief@gmail.com>
2024-08-14 01:03:45 +0700

    [UPDATE] Stories response

--- app/src/main/java/com/zhalz/voyageoflife/data/remote/response/StoriesResponse.kt
+++ app/src/main/java/com/zhalz/voyageoflife/data/remote/response/StoriesResponse.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName
 data class StoriesResponse(
 
 	@field:SerializedName("listStory")
-	val listStory: List<StoryData?>? = null,
+	val listStory: List<StoryData> = emptyList(),
 
 	@field:SerializedName("error")
 	val error: Boolean? = null,
