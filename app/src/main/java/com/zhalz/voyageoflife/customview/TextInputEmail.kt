8121f422752c526945ba95a1234cc3e7405d2380
Faishal Mukhammad Syarief <faishalmukhammadsyarief@gmail.com>
2024-08-13 17:45:53 +0700

    [UPDATE] UI

--- app/src/main/java/com/zhalz/voyageoflife/customview/TextInputEmail.kt
+++ app/src/main/java/com/zhalz/voyageoflife/customview/TextInputEmail.kt
@@ -4,26 +4,27 @@ import android.content.Context
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.AttributeSet
+import android.util.Patterns.EMAIL_ADDRESS
 import com.google.android.material.textfield.TextInputEditText
 import com.zhalz.voyageoflife.R
 
-class EditTextPassword @JvmOverloads constructor(
+class TextInputEmail @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null
 ) : TextInputEditText(context, attrs) {
 
     init {
-        validatePassword()
+        validateEmail()
     }
 
-    private fun validatePassword() = addTextChangedListener(object : TextWatcher {
+    private fun validateEmail() = addTextChangedListener(object : TextWatcher {
 
         override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
         override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
         override fun afterTextChanged(input: Editable) =
-            if (input.length < 8) setError(context.getString(R.string.password_error), null)
+            if (!EMAIL_ADDRESS.matcher(input).matches()) setError(context.getString(R.string.email_error), null)
             else setError(null)
 
     })
--- app/src/main/java/com/zhalz/voyageoflife/customview/TextInputPassword.kt
+++ app/src/main/java/com/zhalz/voyageoflife/customview/TextInputPassword.kt
@@ -4,27 +4,26 @@ import android.content.Context
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.AttributeSet
-import android.util.Patterns.EMAIL_ADDRESS
 import com.google.android.material.textfield.TextInputEditText
 import com.zhalz.voyageoflife.R
 
-class EditTextEmail @JvmOverloads constructor(
+class TextInputPassword @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null
 ) : TextInputEditText(context, attrs) {
 
     init {
-        validateEmail()
+        validatePassword()
     }
 
-    private fun validateEmail() = addTextChangedListener(object : TextWatcher {
+    private fun validatePassword() = addTextChangedListener(object : TextWatcher {
 
         override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
         override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
         override fun afterTextChanged(input: Editable) =
-            if (!EMAIL_ADDRESS.matcher(input).matches()) setError(context.getString(R.string.email_error), null)
+            if (input.length < 8) setError(context.getString(R.string.password_error), null)
             else setError(null)
 
     })
