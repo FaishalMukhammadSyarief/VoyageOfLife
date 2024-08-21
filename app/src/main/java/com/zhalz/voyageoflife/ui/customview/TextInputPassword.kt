9cd5a18f4056a920bd98d12b76bd70378becff0d
Faishal Mukhammad Syarief <faishalmukhammadsyarief@gmail.com>
2024-08-21 14:20:48 +0700

    [UPDATE] Move customview to ui

--- app/src/main/java/com/zhalz/voyageoflife/ui/customview/TextInputPassword.kt
+++ app/src/main/java/com/zhalz/voyageoflife/ui/customview/TextInputPassword.kt
@@ -1,30 +1,29 @@
-package com.zhalz.voyageoflife.customview
+package com.zhalz.voyageoflife.ui.customview
 
 import android.content.Context
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.AttributeSet
-import android.util.Patterns.EMAIL_ADDRESS
 import com.google.android.material.textfield.TextInputEditText
 import com.zhalz.voyageoflife.R
 
-class TextInputEmail @JvmOverloads constructor(
+class TextInputPassword @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null
 ) : TextInputEditText(context, attrs) {
 
     init {
-        validateEmail()
+        validatePassword()
     }
 
-    private fun validateEmail() = addTextChangedListener(object : TextWatcher {
+    private fun validatePassword() = addTextChangedListener(object : TextWatcher {
 
         override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
         override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
         override fun afterTextChanged(input: Editable) =
-            if (!EMAIL_ADDRESS.matcher(input).matches()) setError(context.getString(R.string.email_error), null)
+            if (input.length < 8) setError(context.getString(R.string.password_error), null)
             else setError(null)
 
     })
